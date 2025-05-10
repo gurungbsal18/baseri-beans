288cc7c4339ab31c0386d4cef7f8af0875f6be55
gurungbsal18 <gurungbsal18@gmail.com>
2025-05-10 23:11:35 +0545

    removed unwanted import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import "@/styles/globals.css";
 import "@/styles/scss/custom-style.scss";
 import { Metadata, Viewport } from "next";
-import { Link } from "@heroui/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import MainMenu from "@/components/MainMenu";
 
 export const metadata: Metadata = {
   title: {
